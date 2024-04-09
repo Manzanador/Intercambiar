bd982d1bdf053247284fb1840c583e94bc950017
pc <pc@LAB2-15>
2024-04-09 08:05:24 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Prgram { 
+
+    static void Intercambiar(ref int n1, ref int n2)
+    {
+        int n3 = n1;
+        n1 = n2; 
+        n2 = n3;
+    }
+static void Main(string[] args)
+{
+        int Primero = 1;
+        int Segundo = 2;
+        Intercambiar(ref Primero,ref Segundo);
+    }
+}
