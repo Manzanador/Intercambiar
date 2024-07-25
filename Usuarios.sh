69418d50ac34cf7952594838d888b6fc5acafcdf
Manzanador <164060728+Manzanador@users.noreply.github.com>
2024-07-25 15:26:04 -0300

    Update Usuarios.sh

--- Usuarios.sh
+++ Usuarios.sh
@@ -22,9 +22,7 @@ echo "--------------------------------------------------------------------------
 echo "1) Crear un usuario"
 echo "2) Eliminar un usuario"
 echo "3) Asignar contraseña a un usuario"
-echo "4) Asignar grupo secundario a un usuario"
-echo "5) Eliminar usuario de grupo secundario"
-echo "6) Establecer descripcion de un usuario"
+echo "4) Establecer descripcion de un usuario"
 echo "0) Salir"
 read -p "Ingrese una opcion: " OP
 case $OP in
@@ -68,47 +66,7 @@ else
 echo "El nombre de usuario ingresado no se corresponde con un usuario del sistema o es vacio"
 fi
 sleep 2;;
-4) read -p "Ingrese el nombre del usuario a ingresar en un grupo: " Nom
-if ValidarExistencia /etc/passwd $Nom
-then
-read -p "Ingrese el nombre del grupo: " Grp
-if ValidarExistencia /etc/group $Grp
-then
-if ValidarPertenenciaGrupo $Grp $Nom
-then
-sudo usermod -a -G $Grp $Nom
-echo "Usuario ingresado con exito"
-else
-echo "El usuario ingresado ya pertenece al grupo ingresado"
-fi
-else
-echo "El grupo ingresado no se corresponde con uno del sistema o es vacio"
-fi
-else
-echo "El nombre de usuario ingresado no se corresponde con un usuario del sistema o es vacio"
-fi
-sleep 2;;
-5) read -p "Ingrese el nombre del usuario a eliminar del grupo: " Nom
-if ValidarExistencia /etc/passwd $Nom
-then
-read -p "Ingrese el nombre del grupo al que pertenece: " Grp
-if ValidarExistencia /etc/group $Grp
-then
-if ValidarPertenenciaGrupo $Grp $Nom
-then
-echo "El usuario ingresado no pertenece al grupo ingresado"
-else
-sudo gpasswd -d $Nom $Grp
-echo "Usuario eliminado con exito"
-fi
-else
-echo "El grupo ingresado no se corresponde con uno del sistema o es vacio"
-fi
-else
-echo "El nombre de usuario ingresado no se corresponde con un usuario del sistema o es vacio"
-fi
-sleep 2;;
-6)read -p "Ingrese el usuario cuya descripcion va a establecer: " Nom
+4)read -p "Ingrese el usuario cuya descripcion va a establecer: " Nom
 if ValidarExistencia /etc/passwd $Nom
 then
 read -p "Ingrese la descripcion a asignar: " Desc
