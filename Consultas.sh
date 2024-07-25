7d0eb64b016f9ebbc6857b1ced7bb012a8c9b180
Manzanador <164060728+Manzanador@users.noreply.github.com>
2024-07-25 15:36:11 -0300

    Create Consultas.sh

---
+++ Consultas.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+while [ "$OP" != 0 ]
+do
+echo "--------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "1) Ver lista de usuarios"
+echo "2) Ver lista de grupos con participantes"
+echo "0) Salir"
+read -p "Ingrese una opcion: " OP
+case $OP in
+1)echo "Usuario / Descripcion"
+egrep -w home /etc/passwd | cut -f1,5 -d: | tr : " "
+sleep 5;;
+2)echo "Grupo / Integrantes"
+while var= read line
+do
+if [[ `echo $line | cut -f3 -d:` -ge 1000 ]] && !(ValidarExistencia /etc/passwd `echo $line | cut -f1 -d:`)
+then
+echo $line | cut -f1,4 -d: | tr : " "
+fi
+done < /etc/group
+sleep 5;;
+0)echo Adios;;
+*) echo "La opcion ingresada no es valida"
+sleep 2;;
+esac
+done
